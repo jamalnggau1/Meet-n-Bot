7f431eaa28b8502adad8849154936c1a130f9029
Rico <r.jambor@yahoo.de>
2017-01-23 23:47:17 +0100

    Add message_types
    Message types to determine what the type of a message a user sent is.

---
+++ app/message_types.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class MessageTypes(Enum):
+    TYPE_TEXT = 0
+    TYPE_PHOTO = 1
+    TYPE_STICKER = 2
+    TYPE_GIF = 3
+    TYPE_VOICE = 4
+    TYPE_AUDIO = 4
+    TYPE_VIDEO = 6
