b8331f32033738deb3e864f1e5393642c9fbe706
Dario <dariomsn91@gmail.com>
2017-01-24 19:28:08 +0100

    emojis to distinguish bot and strangers in messages

--- app/main.py
+++ app/main.py
@@ -25,6 +25,8 @@ class Main(object):
                 self.analyze_messages()
 
     def analyze_messages(self):
+        bot_sends = "\U0001F916 *Bot:* "
+        stranger_sends = "\U0001F464 *Stranger:* " 
         try:
             while len(self.left_msgs) > 0:
                 update = self.left_msgs[0]
@@ -47,14 +49,14 @@ class Main(object):
                     message_type = MessageTypes.TYPE_STICKER
 
                 elif update.message.photo is not None:
-                    print("Message is a photo")
+                    print("\U0001F4F7 Message is a photo")
                     print(str(update.message.photo))
                 else:
                     # Setting type to "text", so that the partner get's notified of it
                     print("* Someone sent some unsupported media, sorry. *")
                     message_type = MessageTypes.TYPE_TEXT
-                    text_orig = "* Your chat partner sent some unsupported media, sorry. *"
-                    text = "* Your chat partner sent some unsupported media, sorry. *"
+                    text_orig = bot_sends + "*Your chat partner sent some unsupported media, sorry. *"
+                    text = bot_sends + "*Your chat partner sent some unsupported media, sorry. *"
 
                 user_id = update.message.sender.id
                 # first_name = update.message.sender.first_name
@@ -77,16 +79,16 @@ class Main(object):
                             self.chatting_users.append([user_id, partner_id])
                             self.chatting_users.append([partner_id, user_id])
 
-                            self.bot.send_message(user_id, "You are connected to a stranger. Have fun and be nice!").wait()
-                            self.bot.send_message(partner_id, "You are connected to a stranger. Have fun and be nice!").wait()
+                            self.bot.send_message(user_id, bot_sends + "You are connected to a stranger. Have fun and be nice!").wait()
+                            self.bot.send_message(partner_id, bot_sends + "You are connected to a stranger. Have fun and be nice!").wait()
                         else:
                             # if no user is searching, add him to the list of searching users.
                             # TODO later when you can search for specific gender, this condition must be changed
                             self.searching_users.append(user_id)
-                            self.bot.send_message(user_id, "Added you to the searching users!").wait()
+                            self.bot.send_message(user_id, bot_sends + "Added you to the searching users!").wait()
 
                     elif user_id in self.searching_users:
-                        self.bot.send_message(user_id, "You are already searching. Please wait!").wait()
+                        self.bot.send_message(user_id, bot_sends + "You are already searching. Please wait!").wait()
 
                     if (command == "stop") and ((user_id in self.searching_users) or (self.user_already_chatting(user_id) >= 0)):
 
@@ -106,8 +108,8 @@ class Main(object):
                             del self.chatting_users[partner_index]
 
                             # send message that other user left the chat
-                            self.bot.send_message(partner_id, "Your partner left the chat").wait()
-                            self.bot.send_message(user_id, "You left the chat!").wait()
+                            self.bot.send_message(partner_id, bot_sends + "Your partner left the chat").wait()
+                            self.bot.send_message(user_id, bot_sends + "You left the chat!").wait()
 
                 # if user is in a chat
                 elif (user_id not in self.searching_users) and (self.user_already_chatting(user_id) >= 0):
@@ -116,7 +118,7 @@ class Main(object):
                     # additional check, that there is indeed a partner.
                     if partner_id != -1:
                         if message_type == MessageTypes.TYPE_TEXT:
-                            message = "Stranger: " + text_orig
+                            message = stranger_sends + text_orig
                             self.bot.send_message(partner_id, message).wait()
                         elif message_type == MessageTypes.TYPE_STICKER:
                             self.bot.send_sticker(partner_id, update.message.sticker.file_id).wait()
