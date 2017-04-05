0c07efba7d3bdd1029034fb18a1404dce8ef4bc6
Rico <r.jambor@yahoo.de>
2017-04-05 16:19:28 +0200

    Switch to python-telegram-bot framework

--- app/main.py
+++ app/main.py
@@ -1,199 +1,183 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Rico'
 
-import html
-from twx.botapi import TelegramBot
-from app.update_handler import get_updates
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from app.message_types import MessageTypes
+import logging
 
 
-class Main(object):
-    # BOT_TOKEN = <your-API-Token-here>
-    bot = TelegramBot(BOT_TOKEN)
-    offset = 0
-    left_msgs = [[]] * 0
-    chatting_users = [[]] * 0
-    searching_users = []
-    DEV_ID = 24421134
+# BOT_TOKEN = <your-API-Token-here>
 
-    def update_loop(self):
-        while True:
-            update_list = get_updates(self.offset, self.bot)
-            if update_list:
-                for line in update_list:
-                    self.left_msgs.append(line)
-                self.analyze_messages()
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+updater = Updater(token=BOT_TOKEN)
+dispatcher = updater.dispatcher
+# bot = updater.bot
 
-    def analyze_messages(self):
-        bot_sends = "\U0001F916 *Bot:* "
-        stranger_sends = "\U0001F464 <b>Stranger:</b> " 
-        try:
-            while len(self.left_msgs) > 0:
-                update = self.left_msgs[0]
-                print(update)  # TODO just for debugging
-                text_orig = ""
-                text = ""
-                message_type = 0
+chatting_users = []
+searching_users = []
+DEV_ID = 24421134
+bot_sends = "\U0001F916 Bot: "
+stranger_sends = "\U0001F464 Stranger: "
 
-                #TODO auto remove chats if no message for a certain time?
+def get_partner_id(user_id):
+    if len(chatting_users) > 0:
+        for pair in chatting_users:
+            if pair[0] == user_id:
+                return int(pair[1])
 
-                if isinstance(update.message.text, str):
-                    # If message is text
-                    text_orig = str(update.message.text)
-                    text = text_orig.lower()
-                    message_type = MessageTypes.TYPE_TEXT
-                elif update.message.photo is not None:
-                    message_type = MessageTypes.TYPE_PHOTO
-                elif update.message.sticker is not None:
-                    message_type = MessageTypes.TYPE_STICKER
-                elif update.message.voice is not None:
-                    message_type = MessageTypes.TYPE_VOICE
-                elif update.message.audio is not None:
-                    message_type = MessageTypes.TYPE_AUDIO
-                elif update.message.video is not None:
-                    message_type = MessageTypes.TYPE_VIDEO
-                elif update.message.document is not None:
-                    message_type = MessageTypes.TYPE_FILE
-                else:
-                    # Setting type to "text", so that the partner get's notified of it
-                    print("* Someone sent some unsupported media*")
-                    message_type = MessageTypes.TYPE_TEXT
-                    # TODO here is a potential risk!
-                    text_orig = bot_sends + "*Your chat partner sent some unsupported media, sorry. *"
-                    text = bot_sends + "*Your chat partner sent some unsupported media, sorry. *"
+    return None
 
-                user_id = update.message.sender.id
-                # first_name = update.message.sender.first_name
-                # update_id = update.update_id
+# checks if user is already chatting with someone
+def is_user_already_chatting(user_id):
+    counter = 0
+    if len(chatting_users) > 0:
+        for pair in chatting_users:
+            if pair[0] == user_id:
+                return counter
+            counter += 1
 
-                # If message is a command
-                if text.startswith("/"):
-                    command = str(text[1:])
-                    command_orig = str(text_orig[1:]) # just in case the original text is needed
+    return -1
 
-                    if (command == "start") and (user_id not in self.searching_users) and (self.user_already_chatting(user_id) == -1):
-                        # search for another "searching" user in searching_users list
-                        if len(self.searching_users) > 0:
-                            # delete the other searching users from the list of searching_users
-                            print("Another user is searching now. There are 2 users. Matching them now!")
-                            partner_id = self.searching_users[0]
-                            del self.searching_users[0]
+def is_user_already_searching( user_id):
+    counter = 0
+    if len(searching_users) > 0:
+        for user in searching_users:
+            if user == user_id:
+                return counter
+            counter += 1
 
-                            # add both users to the list of chatting users with the user_id of the other user.
-                            self.chatting_users.append([user_id, partner_id])
-                            self.chatting_users.append([partner_id, user_id])
-
-                            self.bot.send_message(user_id, bot_sends + "You are connected to a stranger. Have fun and be nice!", parse_mode="Markdown").wait()
-                            self.bot.send_message(partner_id, bot_sends + "You are connected to a stranger. Have fun and be nice!", parse_mode="Markdown").wait()
-                        else:
-                            # if no user is searching, add him to the list of searching users.
-                            # TODO later when you can search for specific gender, this condition must be changed
-                            self.searching_users.append(user_id)
-                            self.bot.send_message(user_id, bot_sends + "Added you to the searching users!", parse_mode="Markdown").wait()
-
-                    elif (command == "start") and (user_id in self.searching_users):
-                        self.bot.send_message(user_id, bot_sends + "You are already searching. Please wait!", parse_mode="Markdown").wait()
-                    if (command == "stop") and ((user_id in self.searching_users) or (self.user_already_chatting(user_id) >= 0)):
-
-                        if user_id in self.searching_users:
-                            # remove user from searching users
-                            index = self.user_already_searching(user_id)
-                            del self.searching_users[index]
-
-                        elif self.user_already_chatting(user_id) >= 0:
-                            # remove both users from chatting users
-                            partner_id = self.get_partner_id(user_id)
-
-                            index = self.user_already_chatting(user_id)
-                            del self.chatting_users[index]
-
-                            partner_index = self.user_already_chatting(partner_id)
-                            del self.chatting_users[partner_index]
-
-                            # send message that other user left the chat
-                            self.bot.send_message(partner_id, bot_sends + "Your partner left the chat", parse_mode="Markdown").wait()
-                            self.bot.send_message(user_id, bot_sends + "You left the chat!", parse_mode="Markdown").wait()
-
-                # if user is in a chat
-                elif (user_id not in self.searching_users) and (self.user_already_chatting(user_id) >= 0):
-                    # send message directly to the other chat
-                    partner_id = self.get_partner_id(user_id)
-                    # additional check, that there is indeed a partner.
-                    if partner_id != -1:
-                        if message_type == MessageTypes.TYPE_TEXT:
-                            message = stranger_sends + html.escape(text_orig)
-                            self.bot.send_message(partner_id, message, parse_mode="HTML").wait()
-                        elif message_type == MessageTypes.TYPE_PHOTO:
-                            file_id = ""
-                            last_file_size = 0
-                            for photo in update.message.photo:
-                                if photo.file_size >= last_file_size:
-                                    file_id = photo.file_id
-                            self.bot.send_photo(partner_id, file_id).wait()
-                            print("sent file with size: " + str(last_file_size))
-                        elif message_type == MessageTypes.TYPE_STICKER:
-                            self.bot.send_sticker(partner_id, update.message.sticker.file_id).wait()
-                        elif message_type == MessageTypes.TYPE_VOICE:
-                            self.bot.send_voice(partner_id, update.message.voice.file_id, duration=update.message.voice.duration).wait()
-                        elif message_type == MessageTypes.TYPE_AUDIO:
-                            self.bot.send_audio(partner_id, update.message.audio.file_id).wait()
-                        elif message_type == MessageTypes.TYPE_VIDEO:
-                            self.bot.send_video(partner_id, update.message.video.file_id).wait()
-                        elif message_type == MessageTypes.TYPE_FILE:
-                            self.bot.send_document(partner_id, update.message.document.file_id).wait()
-                    else:
-                        print("Something went wrong! There is no partner in the list, while there should be!")
-
-                # happens when user is NEITER searching, nor in a chat with a stranger
-                else:
-                    print("Case not handeled yet!")
-
-                self.set_message_answered()
-        except Exception as expt:
-            print(expt)
-            self.set_message_answered()
-            self.bot.send_message(self.DEV_ID, "Bot Error:\n\nMessages couldn't be analyzed!")
-            # traceback.print_exc()
-
-    def get_partner_id(self, user_id):
-        if len(self.chatting_users) > 0:
-            for pair in self.chatting_users:
-                if pair[0] == user_id:
-                    return int(pair[1])
-
-        return -1
-
-    # checks if user is already chatting with someone
-    def user_already_chatting(self, user_id):
-        counter = 0
-        if len(self.chatting_users) > 0:
-            for pair in self.chatting_users:
-                if pair[0] == user_id:
-                    return counter
-                counter += 1
-
-        return -1
-
-    def user_already_searching(self, user_id):
-        counter = 0
-        if len(self.searching_users) > 0:
-            for user in self.searching_users:
-                if user == user_id:
-                    return counter
-                counter += 1
-
-        return -1
-
-    def set_message_answered(self):
-        if len(self.left_msgs) > 0:
-            update = self.left_msgs[0]
-            self.offset = update.update_id
-            self.left_msgs.pop(0)
-            print("Un-Answered Messages: " + str(len(self.left_msgs)))
-
-    def __init__(self):
-        print("Bot started!")
+    return -1
 
 
-main = Main()
-main.update_loop()
+def start(bot, update):
+    user_id = update.message.from_user.id
+
+    print("start")
+    if (user_id not in searching_users) and (is_user_already_chatting(user_id) == -1):
+        # search for another "searching" user in searching_users list
+        if len(searching_users) > 0:
+            # delete the other searching users from the list of searching_users
+            print("Another user is searching now. There are 2 users. Matching them now!")
+            partner_id = searching_users[0]
+            del searching_users[0]
+
+            # add both users to the list of chatting users with the user_id of the other user.
+            chatting_users.append([user_id, partner_id])
+            chatting_users.append([partner_id, user_id])
+
+            bot.send_message(user_id, bot_sends + "You are connected to a stranger. Have fun and be nice!",
+                                  parse_mode="Markdown")
+            bot.send_message(partner_id, bot_sends + "You are connected to a stranger. Have fun and be nice!",
+                                  parse_mode="Markdown")
+        else:
+            # if no user is searching, add him to the list of searching users.
+            # TODO later when you can search for specific gender, this condition must be changed
+            searching_users.append(user_id)
+            bot.send_message(user_id, bot_sends + "Added you to the searching users!",
+                                  parse_mode="Markdown")
+    elif user_id in searching_users:
+        bot.send_message(user_id, bot_sends + "You are already searching. Please wait!", parse_mode="Markdown")
+    pass
+
+
+def stop(bot, update):
+    user_id = update.message.from_user.id
+    print("stop")
+
+    if (user_id in searching_users) or (is_user_already_chatting(user_id) >= 0):
+
+        if user_id in searching_users:
+            # remove user from searching users
+            index = is_user_already_searching(user_id)
+            del searching_users[index]
+            bot.send_message(user_id, bot_sends + "Stopped searching for users!", parse_mode="Markdown")
+
+        elif is_user_already_chatting(user_id) >= 0:
+            # remove both users from chatting users
+            partner_id = get_partner_id(user_id)
+
+            index = is_user_already_chatting(user_id)
+            del chatting_users[index]
+
+            partner_index = is_user_already_chatting(partner_id)
+            del chatting_users[partner_index]
+
+            # send message that other user left the chat
+            bot.send_message(partner_id, bot_sends + "Your partner left the chat", parse_mode="Markdown")
+            bot.send_message(user_id, bot_sends + "You left the chat!", parse_mode="Markdown")
+
+
+def messages(bot, update):
+    user_id = update.message.from_user.id
+    text = update.message.text
+    print("message")
+
+    if text is not None and text is not "":
+        # If message is text
+        logger.debug("Update is text")
+        text = str(update.message.text)
+        message_type = MessageTypes.TYPE_TEXT
+    elif update.message.photo is not None and update.message.photo != []:
+        logger.debug("Update is photo")
+        print(update.message.photo)
+        message_type = MessageTypes.TYPE_PHOTO
+    elif update.message.sticker is not None and update.message.sticker != []:
+        logger.debug("Update is sticker")
+        message_type = MessageTypes.TYPE_STICKER
+    elif update.message.voice is not None and update.message.voice != []:
+        logger.debug("Update is voice")
+        message_type = MessageTypes.TYPE_VOICE
+    elif update.message.audio is not None and update.message.audio != []:
+        logger.debug("Update is audio")
+        message_type = MessageTypes.TYPE_AUDIO
+    elif update.message.video is not None and update.message.video != []:
+        logger.debug("Update is video")
+        message_type = MessageTypes.TYPE_VIDEO
+    elif update.message.document is not None and update.message.document != []:
+        logger.debug("Update is document")
+        message_type = MessageTypes.TYPE_FILE
+    else:
+        # Setting type to "text", so that the partner get's notified of it
+        print("* Someone sent some unsupported media*")
+        message_type = MessageTypes.TYPE_TEXT
+
+    if (user_id not in searching_users) and (is_user_already_chatting(user_id) >= 0):
+        # send message directly to the other chat
+        partner_id = get_partner_id(user_id)
+        # additional check, that there is indeed a partner.
+        if partner_id != -1:
+            if message_type == MessageTypes.TYPE_TEXT:
+                message = stranger_sends + text
+                bot.send_message(partner_id, message)
+            elif message_type == MessageTypes.TYPE_PHOTO:
+                file_id = ""
+                last_file_size = 0
+                for photo in update.message.photo:
+                    if photo.file_size >= last_file_size:
+                        file_id = photo.file_id
+                bot.send_photo(partner_id, file_id)
+                print("sent file with size: " + str(last_file_size))
+            elif message_type == MessageTypes.TYPE_STICKER:
+                bot.send_sticker(partner_id, update.message.sticker.file_id)
+            elif message_type == MessageTypes.TYPE_VOICE:
+                bot.send_voice(partner_id, update.message.voice.file_id,
+                                    duration=update.message.voice.duration)
+            elif message_type == MessageTypes.TYPE_AUDIO:
+                bot.send_audio(partner_id, update.message.audio.file_id)
+            elif message_type == MessageTypes.TYPE_VIDEO:
+                bot.send_video(partner_id, update.message.video.file_id)
+            elif message_type == MessageTypes.TYPE_FILE:
+                bot.send_document(partner_id, update.message.document.file_id)
+        else:
+            print("Something went wrong! There is no partner in the list, while there should be!")
+
+
+start_handler = CommandHandler('start', start)
+stop_handler = CommandHandler('stop', stop)
+message_handler = MessageHandler(Filters.all, messages)
+
+for handler in [start_handler, stop_handler, message_handler]:
+    dispatcher.add_handler(handler)
+
+updater.start_polling()
